7373a780a833d44dc48e4d7954d8fd35bfbb34c2
MrMHappy <105713803+MehdiShad@users.noreply.github.com>
2023-08-06 20:40:56 +0330

    fix base user

--- activity_log/users/models.py
+++ activity_log/users/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 from activity_log.common.models import BaseModel
 
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group, Permission
 from django.contrib.auth.models import BaseUserManager as BUM
-from django.contrib.auth.models import PermissionsMixin
-
 
 
 class BaseUserManager(BUM):
-    def create_user(self, email, is_active=True, is_admin=False, password=None):
+    def create_user(self, first_name, last_name, email, is_active=True, is_admin=False, password=None):
         if not email:
             raise ValueError("Users must have an email address")
 
-        user = self.model(email=self.normalize_email(email.lower()), is_active=is_active, is_admin=is_admin)
+        user = self.model(first_name=first_name, last_name=last_name, email=self.normalize_email(email.lower()),
+                          is_active=is_active, is_admin=is_admin)
 
         if password is not None:
             user.set_password(password)
@@ -39,15 +38,22 @@ class BaseUserManager(BUM):
 
 
 class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
-
-    email = models.EmailField(verbose_name="email address",unique=True)
-
+    email = models.EmailField(verbose_name="email address", unique=True)
+    first_name = models.CharField(max_length=250, blank=True, null=True)
+    last_name = models.CharField(max_length=250, blank=True, null=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    username = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
 
     objects = BaseUserManager()
 
     USERNAME_FIELD = "email"
+
+    # Add custom related_name arguments to avoid reverse accessor clashes
+    groups = models.ManyToManyField(Group, related_name='baseuser_set_groups')
+    user_permissions = models.ManyToManyField(Permission, related_name='baseuser_set_permissions')
+
 
     def __str__(self):
         return self.email
@@ -65,9 +71,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user} >> {self.bio}"
-
-
-
-
-
-
