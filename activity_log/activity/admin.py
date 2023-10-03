27fc9522c039a234a9072e4f83685e0d99c5658e
MrMHappy <mehdishad01@gmail.com>
2023-10-03 15:44:25 +0330

    add Creator and LearningResource models to project and change some models too

--- activity_log/activity/admin.py
+++ activity_log/activity/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from activity_log.activity import models
 
+
 # Register your models here.
 
 @admin.register(models.Topic)
@@ -8,23 +9,36 @@ class TopicAdmin(admin.ModelAdmin):
     list_display = ['fa_title', 'en_title']
 
 
-@admin.register(models.ActivityItem)
-class ActivityItemAdmin(admin.ModelAdmin):
+@admin.register(models.Item)
+class ItemAdmin(admin.ModelAdmin):
     list_display = ['fa_title', 'en_title', 'content_type']
 
 
 @admin.register(models.ActivityLog)
 class ActivityLogAdmin(admin.ModelAdmin):
     # todo: how to change format of created_at in admin panel
-    list_display = ['activity_item', 'activity_date', 'plan', 'is_holiday', 'amount', 'unit_of_measure', 'rate']
-    list_editable = ['activity_date', 'is_holiday', 'rate']
+    list_display = ['activity_item', 'is_holiday', 'activity_source', 'learning_resource', 'amount', 'activity_date', 'plan']
+    list_editable = ['activity_date', 'is_holiday', 'learning_resource']
     # list_filter = ['activity_item']
-    search_fields = ['activity_date', ]
+    search_fields = ['activity_date', 'learning_resource', 'activity_source']
 
 
-@admin.register(models.ActivitySource)
-class ActivitySourceAdmin(admin.ModelAdmin):
+@admin.register(models.Source)
+class SourceAdmin(admin.ModelAdmin):
     list_display = ['id', 'fa_name', 'en_name']
     list_editable = ['fa_name', 'en_name']
     list_filter = ['fa_name', 'en_name']
     search_fields = ['fa_name', 'en_name']
+
+
+@admin.register(models.LearningResource)
+class LearningResourceAdmin(admin.ModelAdmin):
+    list_display = ['title', 'item', 'source']
+    list_editable = ['item', 'source']
+
+
+@admin.register(models.Creator)
+class CreatorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'title', 'en_name', 'github_address', 'youtube_address']
+    list_editable = ['title', 'en_name', 'github_address', 'youtube_address']
+    list_display_links = ['id', 'first_name', 'last_name']
--- activity_log/activity/models.py
+++ activity_log/activity/models.py
@@ -6,6 +6,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
 
+
 # Create your models here.
 
 class Topic(BaseModel):
@@ -28,7 +29,7 @@ CONTENT_TYPE_CHOICES = (
 )
 
 
-class ActivityItem(BaseModel):
+class Item(BaseModel):
     class ContentTypeChoices(models.TextChoices):
         learning = 'learning', 'Learning',
         working = 'working', 'Working',
@@ -50,13 +51,14 @@ class ActivityItem(BaseModel):
     def __str__(self):
         return f"{self.fa_title}"
 
+
 UnitOfMeasure_CHOICES = (
     ('page', 'صفحه'),
     ('minute', 'دقیقه'),
 )
 
 
-class ActivitySource(BaseModel):
+class Source(BaseModel):
     fa_name = models.CharField(max_length=255, null=True, blank=True)
     en_name = models.CharField(max_length=255)
     domain = models.CharField(max_length=1000, null=True, blank=True)
@@ -65,12 +67,48 @@ class ActivitySource(BaseModel):
         return self.en_name
 
 
+class LearningResource(BaseModel):
+    title = models.CharField(max_length=255)
+    item = models.ForeignKey(Item, models.CASCADE, null=True, blank=True)
+    source = models.ForeignKey(Source, on_delete=models.CASCADE, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    enrollment_date = models.DateField(null=True, blank=True)
+    creators = models.ManyToManyField('Creator', related_name='learning_resource', null=True, blank=True)
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Creator(BaseModel):
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    en_name = models.CharField(max_length=200, null=True, blank=True)
+    title = models.CharField(max_length=100, null=True, blank=True)
+    github_address = models.URLField(null=True, blank=True)
+    website_address = models.URLField(null=True, blank=True)
+    youtube_address = models.URLField(null=True, blank=True)
+    linkedin_address = models.URLField(null=True, blank=True)
+    linkedin_address = models.URLField(null=True, blank=True)
+    telegram_address = models.URLField(null=True, blank=True)
+    created_resources_creators = models.ManyToManyField('LearningResource', related_name='creator', null=True, blank=True)
+
+    def __str__(self):
+        if self.first_name is not None or self.last_name is not None:
+            return f"{self.first_name} - {self.last_name}"
+        elif self.title is not None:
+            return str(self.title)
+        else:
+            return "unknown"
+
+
 class ActivityLog(BaseModel):
     activity_date = models.DateField(default=timezone.now, null=True, blank=True)
-    activity_item = models.ForeignKey(ActivityItem, on_delete=models.CASCADE)
+    activity_item = models.ForeignKey(Item, on_delete=models.CASCADE)
     purpose = models.ForeignKey(Purpose, on_delete=models.CASCADE, null=True, blank=True)
     plan = models.ForeignKey(Plan, on_delete=models.CASCADE, null=True, blank=True)
-    activity_source = models.ForeignKey(ActivitySource, on_delete=models.PROTECT, null=True, blank=True)
+    activity_source = models.ForeignKey(Source, on_delete=models.PROTECT, null=True, blank=True)
+    learning_resource = models.ForeignKey(LearningResource, on_delete=models.CASCADE, null=True, blank=True)
+    creator = models.ForeignKey(Creator, on_delete=models.CASCADE, null=True, blank=True)
     plan_detail = models.ForeignKey(PlanDetail, on_delete=models.CASCADE, null=True, blank=True)
     base_user = models.ForeignKey(BaseUser, on_delete=models.CASCADE, null=True, blank=True)
     comment = models.TextField(max_length=555, null=True, blank=True)
@@ -92,4 +130,3 @@ def update_plan_progress(sender, instance, **kwargs):
         plan_detail.progress = plan_detail.calculate_progress()
         plan_detail.total_time_spent = plan_detail.calculate_total_time_spent()
         plan_detail.save()
-
