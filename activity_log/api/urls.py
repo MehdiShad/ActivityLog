495a00a2716ca5557a2e16ea743a5ba0ffee092b
MrMHappy <mehdishad01@gmail.com>
2023-11-24 15:03:22 +0330

    feat: Add Authentication

--- activity_log/api/urls.py
+++ activity_log/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('blog/', include(('activity_log.blog.urls', 'blog'))),
+    path('auth/', include(('activity_log.authentication.urls', 'auth'))),
     path('activity/', include(('activity_log.activity.urls', 'activity'))),
 ]
--- activity_log/authentication/urls.py
+++ activity_log/authentication/urls.py
@@ -1,10 +1,11 @@
+from . import views
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
         path('jwt/', include(([
-            path('login/', TokenObtainPairView.as_view(),name="login"),
-            path('refresh/', TokenRefreshView.as_view(),name="refresh"),
-            path('verify/', TokenVerifyView.as_view(),name="verify"),
+            # path('login/', TokenObtainPairView.as_view(),name="login"),
+            path('login/', views.CustomTokenObtainPairView.as_view(),name="login"),
+            path('refresh/', views.CustomTokenRefreshView.as_view(),name="refresh"),
+            path('verify/', views.CustomTokenVerifyView.as_view(),name="verify"),
             ])), name="jwt"),
 ]
